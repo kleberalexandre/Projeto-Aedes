6d2da77be7b912e163ae239850dade682079cb6e
kleberalexandre <kleber.catanduva@gmail.com>
2016-11-17 06:46:56 -0200

    alteracao na tela de login e tratamento salvando

--- src/java/model/TratamentoQuimico.java
+++ src/java/model/TratamentoQuimico.java
@@ -5,11 +5,21 @@
  */
 package model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
 /**
  *
  * @author Kleber
  */
+@Entity
+@Table(name = "tratamentoQuimico")
 public class TratamentoQuimico {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
     private String descricao;
 
